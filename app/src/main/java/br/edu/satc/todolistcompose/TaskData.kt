47c6aa10f807f027189f54e0fdbf1511a1939635
Ana Beatriz Meller <anabea325@gmail.com>
2025-04-08 14:37:43 -0300

    Alterando TaskData

--- app/src/main/java/br/edu/satc/todolistcompose/TaskData.kt
+++ app/src/main/java/br/edu/satc/todolistcompose/TaskData.kt
@@ -1,7 +1,41 @@
 package br.edu.satc.todolistcompose
 
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RoomDatabase
+import androidx.room.Update
+
+
+@Entity(tableName = "task")
 data class TaskData (
-    val title: String,
-    val description: String,
-    val complete: Boolean
+    @PrimaryKey(autoGenerate = true) val uid: Int,
+    @ColumnInfo (name = "title") val title: String,
+    @ColumnInfo (name = "description") val description: String,
+    @ColumnInfo (name = "complete") val complete: Boolean
 )
+
+@Dao
+interface TaskDao {
+    @Query("SELECT * FROM task")
+    fun getAll(): List<TaskData>
+
+    @Update
+    fun updateAll(vararg tasks: TaskData)
+
+    @Insert
+    fun insertAll(vararg tasks: TaskData)
+
+    @Delete
+    fun delete(task: TaskData)
+}
+
+@Database(entities = [TaskData::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userDao(): TaskDao
+}
